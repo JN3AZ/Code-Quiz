118acb42d838cd644e7bad2e11fc5f57fa2cdb4e
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-05-10 21:11:55 -0700

    added starting files, created html structure ie quiz container, added the questions in js script, set up all links

---
+++ Questions.js
@@ -0,0 +1,37 @@
+var questions = [{
+     "question": "Commonly used data types DO NOT include:",
+     "option1": "strings",
+     "option2": "booleans",
+     "option3": "alerts",
+     "option4": "numbers",
+     "answer": "3",
+}, {
+    "question": "The condition in an if / else statement is enclosed within _____.",
+    "option1": "quotes",
+    "option2": "curly brackets",
+    "option3": "parentheses",
+    "option4": "square brackets",
+    "answer": "3",
+}, {
+    "question": "Arrays in JavaScript can be used to store _____.",
+    "option1": "numbers and strings",
+    "option2": "other arrays",
+    "option3": "booleans",
+    "option4": "all of the above",
+    "answer": "4",
+}, {
+    "question": "String Values must be enclosed within _____ when being assigned to variables.",
+    "option1": "commas",
+    "option2": "curly brackets",
+    "option3": "quotes",
+    "option4": "parentheses",
+    "answer": "3",
+},{
+    "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+    "option1": "Javascript",
+    "option2": "terminal/bash",
+    "option3": "for loops",
+    "option4": "console.log",
+    "answer": "4",
+},
+]
